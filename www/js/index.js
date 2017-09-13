e40241f63762e3ec873b7c28b218f3005206aa21
André Sünnemann <a.suennemann@edv-peuker.de>
2017-09-13 23:34:36 +0200

    test 3

--- www/js/index.js
+++ www/js/index.js
@@ -19,7 +19,7 @@ var app = {
     onDeviceReady: function()
     {
         //window.alert(window.location.href);
-        cordova.InAppBrowser.open('http://dev01.managed-solr.de', '_self', 'location=no');
+        cordova.InAppBrowser.open('http://dev01.managed-solr.de', '_self', 'location=no,zoom=no');
         //window.alert(window.location.href);
         //window.location="https://www.edv-peuker.de";
     }
