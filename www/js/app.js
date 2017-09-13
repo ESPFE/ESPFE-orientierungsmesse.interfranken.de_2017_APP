2e02402b5b53bcde5a840c1d13613db8b9e65a54
André Sünnemann <a.suennemann@edv-peuker.de>
2017-09-13 22:26:11 +0200

    first test

---
+++ www/js/app.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+'use strict';
+
+let app = {
+    initialize: function()
+    {
+        
+    },
+    
+    bindEvents: function()
+    {
+        
+    },
+    
+    onDeviceReady: function()
+    {
+        
+    }
+};
+
+app.initialize();
+
+
+function onDeviceReady()
+{
+    if (navigator.connection.type == Connection.NONE)
+    {
+        navigator.notification.alert('An internet connection is required to continue');
+    }
+    else
+    {
+        window.location="http://dev01.managed-solr.de";
+    }
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
